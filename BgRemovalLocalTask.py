861142f2ef198bcaf0823fd1fe876bfc072226e2
softcat477 <softcat477@gmail.com>
2022-05-16 08:43:43 -0400

    unit testing

--- BgRemovalLocalTask.py
+++ BgRemovalLocalTask.py
@@ -1,16 +1,9 @@
 import argparse
 import os.path as osp
-import os
 import glob
-from pathlib import Path
 
-import numpy as np
-from skimage.io import imread, imsave
-import cv2
-# from matplotlib import pyplot as plt
 from background_removal_engine import remove_background
-
-# TODO: import engine
+from LoaderWriter import load_image, write_image
 
 def getArgs():
     parser = argparse.ArgumentParser()
@@ -26,25 +19,6 @@ def getArgs():
                         help="Paramter for saulova algorithm")
     args = parser.parse_args()
     return args
-
-def load_image(image_path):
-    """Load <image_path> as a ndarray with shape (width, height, 3).
-    Only support rgb format.
-
-    Parameters:
-        image_path (str): Load <image path>. The image should be in RGB or RGBA.
-        mode (str): A flag regarding how to load the image. For now it only supports rgb
-    Returns:
-        np.ndarray: Load image with shape (W, H, 3) with dtype=uint8. The channel order is RGB. 
-    Exception:
-        TypeError: Do not try to load a grey scale image.
-    """
-    image_rgb = imread(image_path)
-
-    if image_rgb.dtype != np.uint8:
-        raise TypeError("load_image should return dtype=uint8. Got an image {} with dtype {}".format(image_path, image_rgb.dtype))
-
-    return image_rgb
 
 def loader(args):
     """ A generator to load *.png locating inside <args.dataset_path>.
@@ -62,34 +36,18 @@ def loader(args):
         image_rgb = load_image(image_path)
         yield image_name, image_rgb
 
-def writer(image, image_name, args):
-    """Write <image> to <args.output_path> with a new name. <image name without its filetype><args.output_postfix>.<filetype>
-
-    This function convert RGB to BGR and use opencv to write the image.
-    Sklearn package runs significantly slower than opencv.
-    Make sure the image is in RGB order!
-
-    Parameters:
-        image (np.ndarray): The output image with shape (W, H, 3) with dtype=uint8. The channel order is RGB. 
-        image_name (str): the image_name from the loader function.
-        args (argparse.Namespace): A namespace with arguments: <output_path>, <output_postfix>
-    Returns:
-        None
-    """
-    filename, filetype = image_name.split(".")
-    #new_image_name = f"{filename}{args.output_postfix}.{filetype}"
-    new_image_name = "{}{}.{}".format(filename, args.output_postfix, filetype)
-    output_path = osp.join(args.output_path, new_image_name)
-    print ("Write to {}".format(output_path))
-
-    imsave(output_path, image)
-
 def main():
     args = getArgs()
 
     for image_name, image_rgb in loader(args):
         image_noBg = remove_background(image_rgb, args.window_size, args.k)
-        writer(image_noBg, image_name, args)
+
+        filename, filetype = image_name.split(".")
+        new_image_name = "{}{}.{}".format(filename, args.output_postfix, filetype)
+        output_path = osp.join(args.output_path, new_image_name)
+        print ("Write to {}".format(output_path))
+
+        write_image(output_path, image_noBg)
 
 if __name__ == "__main__":
     main()
